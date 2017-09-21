36ea53f543ec3400460cf5f103db4002433204fb
nehabandal <npbandal@usfca.edu>
2017-09-21 00:58:16 -0700

    Started with DFS, Basic ode for client to chunk file and protobuf added

--- src/main/java/edu/usfca/cs/dfs/Client.java
+++ src/main/java/edu/usfca/cs/dfs/Client.java
@@ -1,10 +1,54 @@
 package edu.usfca.cs.dfs;
 
+import com.google.protobuf.ByteString;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
+
 public class Client {
-
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        chunkFile(new File("/Users/npbandal/BigData/p1-nehabandal/studentOutput8000"));
 
     }
 
+    private static void chunkFile(File file) throws IOException {
+        String fileName = file.getName();
+        int chunkcount = 1;
+        int sizeOfFiles = 1024 * 1024;
+        byte[] buffer = new byte[sizeOfFiles];
+        int bytesAmount = 0;
+        FileInputStream fis = new FileInputStream(file);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+        while ((bytesAmount = bis.read(buffer)) > 0) {
+            String filePartName = String.format("%s_%02d", fileName, chunkcount++);
+            File chunkFile = new File(file.getParent(), filePartName);
+            try (FileOutputStream out = new FileOutputStream(chunkFile)) {
+                out.write(buffer, 0, bytesAmount);
+            }
+            protobuf(chunkFile);
+        }
+    }
+
+    private static void protobuf(File chunkFile) throws IOException {
+        Socket sock = new Socket("localhost", 9999);
+        byte[] bytesFile = Files.readAllBytes(chunkFile.toPath());
+        ByteString data = ByteString.copyFrom(bytesFile);
+
+        StorageMessages.StoreChunk storeChunkMsg = StorageMessages.StoreChunk.newBuilder()
+                .setFileName("chunkFile.txt")
+                .setChunkId(3)
+                .setData(data)
+                .build();
+
+        StorageMessages.StorageMessageWrapper msgWrapper =
+                StorageMessages.StorageMessageWrapper.newBuilder()
+                        .setStoreChunkMsg(storeChunkMsg)
+                        .build();
+        msgWrapper.writeDelimitedTo(sock.getOutputStream());
+
+        sock.close();
+    }
 }
+
+
--- src/main/java/edu/usfca/cs/dfs/StorageNode.java
+++ src/main/java/edu/usfca/cs/dfs/StorageNode.java
@@ -1,14 +1,38 @@
 package edu.usfca.cs.dfs;
 
 import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class StorageNode {
+
+    private ServerSocket srvSocket;
 
     public static void main(String[] args) 
     throws Exception {
         String hostname = getHostname();
         System.out.println("Starting storage node on " + hostname + "...");
+        new StorageNode().start();
+    }
+
+    public void start()
+    throws Exception {
+        srvSocket = new ServerSocket(9999);
+        System.out.println("Listening...");
+        while (true) {
+            Socket socket = srvSocket.accept();
+            StorageMessages.StorageMessageWrapper msgWrapper
+                = StorageMessages.StorageMessageWrapper.parseDelimitedFrom(
+                        socket.getInputStream());
+
+            if (msgWrapper.hasStoreChunkMsg()) {
+                StorageMessages.StoreChunk storeChunkMsg
+                    = msgWrapper.getStoreChunkMsg();
+                System.out.println("Storing file name: "
+                        + storeChunkMsg.getFileName());
+            }
+        }
     }
 
     /**
