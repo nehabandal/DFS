1222c0750aa2dda7562577caf0bf2f867e71a58f
nehabandal <npbandal@usfca.edu>
2017-10-03 22:45:10 -0700

    Recent changes

--- src/main/java/edu/usfca/cs/dfs/Controller.java
+++ src/main/java/edu/usfca/cs/dfs/Controller.java
@@ -1,6 +1,10 @@
 package edu.usfca.cs.dfs;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,5 +16,19 @@ public class Controller extends ProtoBuf {
         System.out.println("Controller listening on port 9998...");
         pb.protoBufToReceiveRequestFromClientAtController(9998, "Request received from client ");
         pb.protoBufToSendResponseToClientFromController(9999, hostNames);
+
+        StorageNode server = new StorageNode(9000);
+        new Thread(server).start();
+
+        try {
+            Thread.sleep(10 * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Stopping Server");
+        server.stop();
+
     }
+
 }
--- src/main/java/edu/usfca/cs/dfs/StorageNode.java
+++ src/main/java/edu/usfca/cs/dfs/StorageNode.java
@@ -1,16 +1,99 @@
 package edu.usfca.cs.dfs;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class StorageNode extends ProtoBuf {
+public class StorageNode extends ProtoBuf implements Runnable {
+
+    protected int serverPort = 8080;
+    protected ServerSocket serverSocket = null;
+    protected boolean isStopped = false;
+    protected Thread runningThread = null;
+
+
     public static void main(String[] args)
             throws Exception {
         String hostname = getHostname();
         ProtoBuf pb = new ProtoBuf();
         System.out.println("Starting storage node on " + hostname + "...");
         pb.protoBufToReceiveRequestFromClientAtStorageNode(9990, "File received ");
+
     }
+
+    public StorageNode(int port) {
+        this.serverPort = port;
+    }
+
+    @Override
+    public void run() {
+        synchronized (this) {
+            this.runningThread = Thread.currentThread();
+        }
+        openServerSocket();
+
+        while (!isStopped()) {
+            Socket clientSocket = null;
+            try {
+                clientSocket = this.serverSocket.accept();
+            } catch (IOException e) {
+                if (isStopped()) {
+                    System.out.println("Server Stopped.");
+                    return;
+                }
+                throw new RuntimeException(
+                        "Error accepting client connection", e);
+            }
+            try {
+                processClientRequest(clientSocket);
+            } catch (IOException e) {
+                //log exception and go on to next request.
+            }
+        }
+
+        System.out.println("Server Stopped.");
+    }
+
+    private void processClientRequest(Socket clientSocket)
+            throws IOException {
+        InputStream input = clientSocket.getInputStream();
+        OutputStream output = clientSocket.getOutputStream();
+        long time = System.currentTimeMillis();
+
+        output.write(("HTTP/1.1 200 OK\n\n<html><body>" +
+                "Singlethreaded Server: " +
+                time +
+                "</body></html>").getBytes());
+        output.close();
+        input.close();
+        System.out.println("Request processed: " + time);
+    }
+
+    private synchronized boolean isStopped() {
+        return this.isStopped;
+    }
+
+    public synchronized void stop() {
+        this.isStopped = true;
+        try {
+            this.serverSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Error closing server", e);
+        }
+    }
+
+    private void openServerSocket() {
+        try {
+            this.serverSocket = new ServerSocket(this.serverPort);
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot open port 8080", e);
+        }
+    }
+
 
     /**
      * Retrieves the short host name of the current host.
